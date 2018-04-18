eba983c8a761807bdb20e2c851539c87237bb3fa
Elizabeth Sibert <github@elizabethsibert.com>
2018-04-18 10:10:44 -0400

    Cleaned and updated code to include el and ea functions and dummy meterological data

--- Farquhar_notes.R
+++ Farquhar_notes.R
@@ -5,57 +5,64 @@
 #     (Vcmax * (Ci - comp)) / (Ci + K1*(1+K2)) - Rday
 
 
-A1<- function(Ci = Ci, Vmo = 35, Tvlo = 300, Tv = Tv, gamma = 0.015) {
+
+A1<- function(Ci, Tv, Vmo = 35, Tvlo = 300, gamma = 0.015) {
    VCmax_fixedT <- Vcmax(Vmo = Vmo, Tvlo = Tvlo, Tv = Tv)
    Rday <- gamma * VCmax_fixedT
-   
    A <- (VCmax_fixedT * (Ci - comp(Tv))) / (Ci + K1(Tv)*(1+K2(Tv))) - Rday
    return(A)
 }
 
-# A2 = alpha * PAR * ((Ci - comp) / (Ci + 2 * comp)) - Rday
 
-A2 <- function(Ci = Ci, PAR = 1000, Vmo = 35, Tvlo = 300, Tv = Tv, alpha = 0.06, gamma = 0.015) {
+# A2 = alpha * PAR * ((Ci - comp) / (Ci + 2 * comp)) - Rday
+# alpha = quantum yield = 0.04
+
+A2 <- function(Ci, Tv, PAR = 1000, Vmo = 35, Tvlo = 300, alpha = 0.04, gamma = 0.015) {
    VCmax_fixedT <- Vcmax(Vmo = Vmo, Tvlo = Tvlo, Tv = Tv)
    Rday <- gamma * VCmax_fixedT
-   
    A <- alpha * PAR * ((Ci - comp(Tv)) / (Ci + 2 * comp(Tv))) - Rday
    return(A)
 }
 
-Amin <- function (Ci, PAR, Vmo, Tvlo, Tv, gamma = 0.015, alpha = 0.06) {
+Amin <- function (Ci, Tv, PAR = 100, Vmo = 35, Tvlo = 300, gamma = 0.015, alpha = 0.04) {
    A1val <- A1(Ci = Ci, Vmo = Vmo, Tvlo = Tvlo, Tv = Tv, gamma = gamma)
    A2val <- A2(Ci = Ci, PAR = PAR, Vmo = Vmo, Tvlo = Tvlo, Tv = Tv, alpha = alpha, gamma = gamma)
    minval <- min(A1val, A2val)
+   return(minval)
 }
 
-   
+Amin(Ci, Tv = 300)
 
 
 ### Equation 2: Stomatal conductance Model
 # gsw = ((M * A) / ((Cs - comp) * (1 + (el - es)/Do))) + b    # open stomata
 #       b                                                     # closed stomata
-
 # b <- function (Tv) {}
 
-gsw <- function(M = const1, Do = cons2, b = const3, Cs = Cons4, Tv = Tv, open = TRUE) {
+gsw <- function(M, Do, b, Ca, Tv, open = TRUE) {
    if(open == TRUE) {
-      gsw <- ((M * Amin) / ((Cs - comp(Tv)) * (1 + (el - es)/Do))) + b
+      gsw <- ((M * Amin) / ((Ca - comp(Tv)) * (1 + (el - es)/Do))) + b
    }
    else {
       gsw <- b
    }
 }
 
+# M = stomatal slope
+# b = cuticular conductance
+# Ca = atmospheric composition of CO2
 
 
 ## Equation 3: 
-# A = -gsw / 1.6 * (Ci  - Ca)
+# A = (-gsw / 1.6) * (Ci  - Ca)
 
 ## Equation 4: Water flux
 # Phi = gsw * (el - ea) 
 
 # Solve for: A, Ci, gsw, Phi
+
+
+
 
 
 #### Parameters we can calculate or get trait values for: 
@@ -82,7 +89,29 @@ K2 <- function(Tv) {0.836 * exp(-1400 * ((1/288.15) - (1/Tv))) }
 # Rday = gamma * Vcmax # ?? gamma = 0.015?
 
 # el # saturated leaf concentration - 100
-# ea # ea = atmospheric water concentration - meterology?
+# ea = atmospheric water concentration - meterology?
+# Saturation vapor pressure equation: el = a * exp(b*T/(T+c)) 
+   # T is temperature (degrees C) of the *leaf*
+   # a = 0.611 kPa
+   # b = 17.502
+   # c = 240.97 degrees C
+# function to convert kelvin to celcius for input into the saturated leaf function (el)
+convertTv <- function(Tv) {
+   TvC <- Tv -273.15
+   return(TvC)
+}
+
+el <- function(Tv) {
+   TvC <- convertTv(Tv)
+   el <- 0.611 * exp((17.502 * TvC)/(TvC + 240.97))
+   return(el)
+}
+
+ea <- function (relHum, Tv) {
+   relHum / el(Tv)
+}
+
+
 # PAR - photon flux density, 500-2000
 # J = 0 - 250 (put alpha*PAR instead of J)
 # Ca = [atm CO2]
@@ -94,13 +123,26 @@ Tv  = seq(274, 315, 1)
 Ci = seq(100, 400, 1)
 
 
-plot(Ci, A1(Ci))
+plot(Ci, A1(Ci, Tv = 298))
 
 
-plot(Ci, A2(Ci))
+plot(Ci, A2(Ci, Tv = 298))
 
 
 points(Ci, A1(Ci))
 plot(Ci, A2(Ci), col='red', ylim=c(0, max(A2(Ci))))
 
 plot(Tv, Vcmax(Vmo = 30, Tvlo = 300, Tv = Tv), col='blue')
+
+
+
+##### Dummy data #####
+dummy <- data.frame(time = c(0:23))
+dummy$Tv <- c(seq(280,300, length.out = 12), seq(300, 280, length.out = 12))
+dummy$relHum <- c(70, 70, 70, 80, 90, 100, 100, 100, 90, 80, 70, 60, 50, 40, 50, 60, rep(70, 8))
+dummy$comp <- comp(Tv = dummy$Tv)
+dummy$Vcmax <- Vcmax(Vmo = 35, Tvlo = 300, Tv = dummy$Tv)
+dummy$el <- el(Tv = dummy$Tv)
+dummy$ea <- ea(relHum = dummy$relHum, tV = dummy$Tv)
+dummy$Ca <- rep(400, 24)
+
