707a99b6b98062f00552d90788bca7e3becbe272
Elizabeth Sibert <github@elizabethsibert.com>
2018-04-18 15:54:24 -0400

    Afternoon post-class notes, includes new VCmax equation

--- Farquhar_notes.R
+++ Farquhar_notes.R
@@ -1,26 +1,28 @@
 ##### Farquhar Modeling equations
 
 ### Equation 1: Farquhar Model - take minimum of light-limited vs. temperature limited
-# A1 = (Vcmax * (Ci-comp)) / (Ci + Kc * (1 + O/K0)) - Rday   # units: umol CO2 / m2 / sec
-#     (Vcmax * (Ci - comp)) / (Ci + K1*(1+K2)) - Rday
+# A1 = (Vcmax * (Ci-comp)) / (Ci + Kc * (1 + O/K0)) - Rd   # units: umol CO2 / m2 / sec
+#     (Vcmax * (Ci - comp)) / (Ci + K1*(1+K2)) - Rd
 
+# Rd = gamma * vcmax; gamma = 0.015
+# vcmax in this context is at 25 C 
 
 
 A1<- function(Ci, Tv, Vmo = 35, Tvlo = 300, gamma = 0.015) {
    VCmax_fixedT <- Vcmax(Vmo = Vmo, Tvlo = Tvlo, Tv = Tv)
-   Rday <- gamma * VCmax_fixedT
-   A <- (VCmax_fixedT * (Ci - comp(Tv))) / (Ci + K1(Tv)*(1+K2(Tv))) - Rday
+   Rd <- gamma * VCmax_fixedT
+   A <- (VCmax_fixedT * (Ci - comp(Tv))) / (Ci + K1(Tv)*(1+K2(Tv))) - Rd
    return(A)
 }
 
 
-# A2 = alpha * PAR * ((Ci - comp) / (Ci + 2 * comp)) - Rday
+# A2 = alpha * PAR * ((Ci - comp) / (Ci + 2 * comp)) - Rd
 # alpha = quantum yield = 0.04
 
 A2 <- function(Ci, Tv, PAR = 1000, Vmo = 35, Tvlo = 300, alpha = 0.04, gamma = 0.015) {
    VCmax_fixedT <- Vcmax(Vmo = Vmo, Tvlo = Tvlo, Tv = Tv)
-   Rday <- gamma * VCmax_fixedT
-   A <- alpha * PAR * ((Ci - comp(Tv)) / (Ci + 2 * comp(Tv))) - Rday
+   Rd <- gamma * VCmax_fixedT
+   A <- alpha * PAR * ((Ci - comp(Tv)) / (Ci + 2 * comp(Tv))) - Rd
    return(A)
 }
 
@@ -34,23 +36,24 @@ Amin <- function (Ci, Tv, PAR = 100, Vmo = 35, Tvlo = 300, gamma = 0.015, alpha 
 Amin(Ci, Tv = 300)
 
 
-### Equation 2: Stomatal conductance Model
+### Equation 2: Stomatal conductance Model (Leunning)
 # gsw = ((M * A) / ((Cs - comp) * (1 + (el - es)/Do))) + b    # open stomata
 #       b                                                     # closed stomata
 # b <- function (Tv) {}
 
-gsw <- function(M, Do, b, Ca, Tv, open = TRUE) {
+gsw <- function(M = 9, Do = 1.3, b = 2, Ca, Tv, open = TRUE) {
    if(open == TRUE) {
-      gsw <- ((M * Amin) / ((Ca - comp(Tv)) * (1 + (el - es)/Do))) + b
+      gsw <- ((M * Amin) / ((Ca - comp(Tv)) * (1 + (el - ea)/Do))) + b
    }
    else {
       gsw <- b
    }
 }
 
-# M = stomatal slope
-# b = cuticular conductance
+# M = stomatal slope = 9
+# b = cuticular conductance = 2 (Anfodillo et al 2002, Tree Physiology)
 # Ca = atmospheric composition of CO2
+# Do = 1.3 kPa, (Launiainen et al 2011, Agricultural and Forest Meterology)
 
 
 ## Equation 3: 
@@ -62,9 +65,6 @@ gsw <- function(M, Do, b, Ca, Tv, open = TRUE) {
 # Solve for: A, Ci, gsw, Phi
 
 
-
-
-
 #### Parameters we can calculate or get trait values for: 
 # Vcmax = (Vmo * exp(3000*(1/288.15 -1/Tv))) / ((1+exp(.4(Tvlo - Tv))) * (1 + exp(0.4 * (Tv - 318.15))))
 Vcmax <- function (Vmo, Tvlo, Tv) {  #Vmo, Tvlo are constants 
@@ -73,6 +73,15 @@ Vcmax <- function (Vmo, Tvlo, Tv) {  #Vmo, Tvlo are constants
 # Vmo = Vc at 25C or 15C  #Vcmax should be ~40 at 25 degrees
 # Tvlo = Low temperature threshold (Kelvin)
 # Tv = leaf temperature (Kelvin)
+
+## new Vcmax equation (for Vcmax at 25c):
+
+# Vcmax = Vmo * exp((36380 * (Tv - 298))/(298*8.314*Tv))
+# Vmo is between 58 and 92, for different connifers
+Vcmax <- function (Vmo, Tv) {
+   Vmo * exp((36380 * (Tv - 298)) / (298 * 8.314 * Tv))
+}
+
 
 # comp = 21.2 * exp(5000 * ((1/288.15) - (1/Tv)))  #CO2 compensation point where photosynthesis = respiration 
 comp <- function(Tv) { 21.2 * exp(5000 * ((1/288.15) - (1/Tv))) } #units: umol/mol
@@ -86,7 +95,7 @@ K2 <- function(Tv) {0.836 * exp(-1400 * ((1/288.15) - (1/Tv))) }
 
 # alpha = 0.06 # mol CO2 / mol photons   ## quantum efficiency
 
-# Rday = gamma * Vcmax # ?? gamma = 0.015?
+# Rd = gamma * Vcmax # ?? gamma = 0.015?
 
 # el # saturated leaf concentration - 100
 # ea = atmospheric water concentration - meterology?
@@ -116,7 +125,6 @@ ea <- function (relHum, Tv) {
 # J = 0 - 250 (put alpha*PAR instead of J)
 # Ca = [atm CO2]
 
-# Do = optimized 
 
 ##### Testing Farquhar (Equation 1 above) 
 Tv  = seq(274, 315, 1)
@@ -139,10 +147,25 @@ plot(Tv, Vcmax(Vmo = 30, Tvlo = 300, Tv = Tv), col='blue')
 ##### Dummy data #####
 dummy <- data.frame(time = c(0:23))
 dummy$Tv <- c(seq(280,300, length.out = 12), seq(300, 280, length.out = 12))
-dummy$relHum <- c(70, 70, 70, 80, 90, 100, 100, 100, 90, 80, 70, 60, 50, 40, 50, 60, rep(70, 8))
+dummy$relHum <- c(rep(70, 3), 80, 80, 90, 100, 100, 90, 80, 70, 60, 50, 40, 50, 60, rep(70, 8))
+dummy$Ca <- rep(400, 24)
 dummy$comp <- comp(Tv = dummy$Tv)
 dummy$Vcmax <- Vcmax(Vmo = 35, Tvlo = 300, Tv = dummy$Tv)
 dummy$el <- el(Tv = dummy$Tv)
-dummy$ea <- ea(relHum = dummy$relHum, tV = dummy$Tv)
-dummy$Ca <- rep(400, 24)
+dummy$ea <- ea(relHum = dummy$relHum, Tv = dummy$Tv)
+
+
+##### Real data #####
+dat <- read.csv('Aggregated_Climate_Data.csv', header=TRUE)
+dat$Tv <- dat$Air_Temp_K 
+dat$relhum <- dat$Relative_Humidity_Percent
+dat$Ca <- dat$co2
+dat$comp <- comp(Tv = dat$Tv)
+dat$Vcmax <- Vcmax(Vmo = 35, Tvlo = 300, Tv = dat$Tv)
+dat$el <- el(Tv = dat$Tv)
+dat$ea <- ea(relHum = dat$relhum, Tv = dat$Tv)
+dat$PAR <- dat$Par_moles_m2_s
+
+
+##### For case where stomata are closed #####
 
